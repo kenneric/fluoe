68ef09a6e540689c41f8a6798d2c4e5eca61f788
kenneric <erickennedy@rogers.com>
2024-11-11 18:24:52 -0500

    title

--- fluoe/server.js
+++ fluoe/server.js
@@ -15,7 +15,7 @@ function renderFullPage(html, css) {
     <html lang="en">
       <head>
         <meta charset="utf-8" />
-        <title>My page</title>
+        <title>Fluoe</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
